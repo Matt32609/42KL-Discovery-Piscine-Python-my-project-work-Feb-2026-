048b76c3be3238832fba7d9b5a1961a1f74d876e
Matt32609 <vthematt@gmail.com>
2026-02-10 14:54:23 +0800

    Add files via upload

---
+++ module3/advanced_mult.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+i = 0
+while i <= 10:
+        print(f"Table of {i}:", end= " ")
+        j = 0
+        while j <= 10:
+                print(i * j, end="")
+                if j < 10:
+                        print(" ", end="")
+                j = j + 1
+        print()
+        i = i + 1
