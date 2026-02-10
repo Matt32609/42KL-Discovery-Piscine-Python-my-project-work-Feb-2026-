eaa9c2b53705513f8e71ffd3bafd4d19890ca8d6
Matt32609 <vthematt@gmail.com>
2026-02-10 14:56:41 +0800

    Add files via upload

---
+++ module4/float.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+number = float(input("Please insert a number :"))
+
+if number % 1 == 0:
+        print("This number is an integer.")
+else:
+        print("This number is a decimal.")
