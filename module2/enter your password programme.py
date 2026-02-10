2c7ffa05eeef7d02280797b020ec984a10df3e35
Matt32609 <vthematt@gmail.com>
2026-02-10 14:51:01 +0800

    Add files via upload

---
+++ module2/enter your password programme.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+password = "Python is awesome"
+
+pw = str(input("Enter the password :"))
+
+if pw == password:
+        print("Access granted.")
+else:
+        print("Access denied.")
