263ea07d1b3fa049e2b60c19b6255249e783aae9
Matt32609 <vthematt@gmail.com>
2026-02-10 14:55:56 +0800

    Add files via upload

---
+++ module4/age.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+age = int(input("Please type your age :"))
+
+print(f"You are now {age} years old.")
+print(f"In 10 years , you will be {age + 10} years old.")
+print(f"In 20 years , you will be {age + 20} years old.")
+print(f"In 30 years , you will be {age + 30} years old.")
