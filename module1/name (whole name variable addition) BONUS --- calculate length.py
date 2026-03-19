2ab474260e27c95a4aa98cb27daccc1ae055075b
Matt32609 <vthematt@gmail.com>
2026-03-19 22:44:13 +0800

    Add files via upload

---
+++ module1/name (whole name variable addition) BONUS --- calculate length.py
@@ -0,0 +1,9 @@
+first_name = ("Matthew")
+last_name = ("Kam")
+whole_name = ("Matthew Kam")
+
+print(whole_name)
+
+print(f"Number of First name : {len(first_name)}")
+print(f"Number of Last name : {len(last_name)}")
+print(f"Number of Whole name : {len(whole_name)}")
