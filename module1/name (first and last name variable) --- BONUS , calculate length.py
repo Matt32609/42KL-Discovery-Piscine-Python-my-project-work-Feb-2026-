c075c2a8a2bd79926b4a244874688aa5317097f5
Matt32609 <vthematt@gmail.com>
2026-03-19 22:44:55 +0800

    Add files via upload

---
+++ module1/name (first and last name variable) --- BONUS , calculate length.py
@@ -0,0 +1,7 @@
+first_name = ("Matthew")
+last_name = ("Kam")
+
+print(first_name , last_name)
+
+print(f"Number of First name : {len(first_name)}")
+print(f"Number of Last name : {len(last_name)}")
